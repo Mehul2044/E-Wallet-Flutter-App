0538a3a87bd0dc8c064b5cfaede6d98c8f050115
Mehul Agarwal <mehu2003@gmail.com>
2024-03-22 17:03:36 +0530

    download feature added in docs

--- lib/widgets/documents/document_widget.dart
+++ lib/widgets/documents/document_widget.dart
@@ -1,9 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
 import 'package:open_file_plus/open_file_plus.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import '../../provider/document_provider.dart';
+
+enum Options { rename, download }
 
 class DocumentWidget extends StatelessWidget {
   final Document documentObj;
@@ -20,6 +27,30 @@ class DocumentWidget extends StatelessWidget {
         return Icons.image;
       default:
         return Icons.insert_drive_file;
+    }
+  }
+
+  Future<bool> _saveFile(File file, String fileName) async {
+    final permission =
+        await Permission.storage.request().isGranted;
+    if (permission) {
+      String newPath = '';
+      Directory storageDir = (await getExternalStorageDirectory())!;
+      List<String> folders = storageDir.path.split("/");
+      for (int x = 1; x < folders.length; x++) {
+        String folder = folders[x];
+        if (folder != 'Android') {
+          newPath += '/$folder';
+        } else {
+          break;
+        }
+      }
+      newPath = "$newPath/Documents";
+      await Directory(newPath).create();
+      await file.copy("$newPath/$fileName");
+      return true;
+    } else {
+      return false;
     }
   }
 
@@ -47,7 +78,45 @@ class DocumentWidget extends StatelessWidget {
         },
         title: Text(fileName, maxLines: 1, overflow: TextOverflow.ellipsis),
         leading: Icon(iconData),
-        subtitle: Text(_formatDateTime(documentObj.dateTime)),
+        subtitle: Text(
+          _formatDateTime(documentObj.dateTime),
+          style: Theme.of(context).textTheme.labelSmall,
+        ),
+        trailing: PopupMenuButton(
+          onSelected: (Options selectedValue) async {
+            if (selectedValue == Options.download) {
+              final result = await _saveFile(documentObj.file, fileName);
+              if (result) {
+                Fluttertoast.showToast(msg: "File was saved in Documents.");
+              } else {
+                Fluttertoast.showToast(msg: "Permission Denied for saving File. Grant permission in the App Settings.");
+              }
+            } else if (selectedValue == Options.rename) {}
+          },
+          itemBuilder: (_) => [
+            const PopupMenuItem(
+              value: Options.rename,
+              child: Row(
+                children: [
+                  Icon(Icons.drive_file_rename_outline),
+                  SizedBox(width: 10),
+                  Text("Rename File"),
+                ],
+              ),
+            ),
+            const PopupMenuItem(
+              value: Options.download,
+              child: Row(
+                children: [
+                  Icon(Icons.download),
+                  SizedBox(width: 10),
+                  Text("Download"),
+                ],
+              ),
+            ),
+          ],
+          icon: const Icon(Icons.more_vert),
+        ),
       ),
     );
   }
