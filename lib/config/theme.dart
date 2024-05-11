e4ac6ccc5397b863820e40308010bb5ee29ee2ce
Mehul Agarwal <mehu2003@gmail.com>
2024-05-11 23:13:17 +0530

    change some issue regarding theming of appbar

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
-const appBarTheme = AppBarTheme(titleTextStyle: TextStyle(fontSize: 17));
+AppBarTheme appbarTheme(bool isDark) {
+  return AppBarTheme(
+    titleTextStyle: TextStyle(
+      color: isDark ? Colors.white : Colors.black,
+      fontSize: 17,
+    ),
+  );
+}
 
 class AppTheme {
-  static final lightTheme =
-      ThemeData.light(useMaterial3: true).copyWith(appBarTheme: appBarTheme);
-  static final darkTheme =
-      ThemeData.dark(useMaterial3: true).copyWith(appBarTheme: appBarTheme);
+  static final lightTheme = ThemeData.light(useMaterial3: true)
+      .copyWith(appBarTheme: appbarTheme(false));
+  static final darkTheme = ThemeData.dark(useMaterial3: true)
+      .copyWith(appBarTheme: appbarTheme(true));
 }
